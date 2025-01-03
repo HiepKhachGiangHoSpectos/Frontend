bb2f3b17cc6505a80de4900a70e4b0ff2ade5dc7
DOCBUOC <baoquanthieuhiep@gmail.com>
2025-01-03 13:39:42 +0700

    ttttttttttttt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -28,7 +27,6 @@ import { httpInterceptorProviders } from './_helpers/http.interceptor';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
     HttpClientModule
   ],
